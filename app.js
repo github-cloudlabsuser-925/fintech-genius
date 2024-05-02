6b3eb67e40519b4445c34b7d54a68068ed83565d
G_Carey <github_cloudlabsuser_922@cloudlabsaiuser.com>
2024-05-02 20:44:13 +0000

    updated gitignore, added ai call, added debug config

--- app.js
+++ app.js
@@ -2,26 +2,35 @@
 const express = require('express');
 const cosmos = require('@azure/cosmos');
 const session = require('express-session');
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 
 //crypto module
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
-const key1= Buffer.from('', 'utf8');
+const key1 = Buffer.from('', 'utf8');
 const iv = Buffer.from('', 'utf8');
-
 
 const { check, validationResult } = require('express-validator');
 
+// Set the Azure and AI Search values from environment variables
+const openaiendpoint = process.env["AZURE_OPENAI_ENDPOINT"];
+const azureApiKey = process.env["AZURE_OPENAI_API_KEY"];
+const deploymentId = process.env["AZURE_OPENAI_DEPLOYMENT_ID"];
+const searchEndpoint = process.env["AZURE_AI_SEARCH_ENDPOINT"];
+const searchKey = process.env["AZURE_AI_SEARCH_API_KEY"];
+const searchIndex = process.env["AZURE_AI_SEARCH_INDEX"];
+
+const aiClient = new OpenAIClient(openaiendpoint, new AzureKeyCredential(azureApiKey));
 
 // Initialize express app
 const app = express();
 app.use(express.json());
 
 // Cosmos DB connection
-const endpoint = "https://fintech-credit-card.documents.azure.com:443/";
+const cosmodbendpoint = "https://fintech-credit-card.documents.azure.com:443/";
 const key = "";
 const { CosmosClient } = cosmos;
-const client = new CosmosClient({ endpoint, key });
+const client = new CosmosClient({ endpoint: cosmodbendpoint, key });
 const database = client.database('credit_cards');
 const container = database.container('credit_card_users');
 
@@ -34,7 +43,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: true } // Note: secure: true option requires an HTTPS connection
-  }));
+}));
 
 // Login endpoint
 app.post('/login', async (req, res) => {
@@ -58,47 +67,47 @@ app.post('/login', async (req, res) => {
 });
 
 // Create account endpoint
-app.post('/create_account', 
-[
-    check('login.username').isLength({ min: 5 }).withMessage('Username must be at least 5 chars long'),
-    check('login.password').isLength({ min: 5 }).withMessage('Password must be at least 5 chars long'),
-    check('login.email').isEmail().withMessage('Email is not valid'),
-    check('first_name').exists().withMessage('First name is required'),
-  ],
-  async (req, res) => {
-    const { login,  first_name, last_name, address } = req.body;
-    const { username, password, email } = login;
-    console.log(username);
-    console.log(username.length);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+app.post('/create_account',
+    [
+        check('login.username').isLength({ min: 5 }).withMessage('Username must be at least 5 chars long'),
+        check('login.password').isLength({ min: 5 }).withMessage('Password must be at least 5 chars long'),
+        check('login.email').isEmail().withMessage('Email is not valid'),
+        check('first_name').exists().withMessage('First name is required'),
+    ],
+    async (req, res) => {
+        const { login, first_name, last_name, address } = req.body;
+        const { username, password, email } = login;
+        console.log(username);
+        console.log(username.length);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    const { resources } = await container.items
-        .query({ query: "SELECT * FROM c WHERE c.login.username = @username", parameters: [{ name: "@username", value: username }] })
-        .fetchAll();
-    if (resources.length > 0) {
-        res.json({ success: false, message: 'Username already exists' });
-    } else {
-        console.log(`Password: ${password}, Salt Rounds: ${saltRounds}`);
+        const { resources } = await container.items
+            .query({ query: "SELECT * FROM c WHERE c.login.username = @username", parameters: [{ name: "@username", value: username }] })
+            .fetchAll();
+        if (resources.length > 0) {
+            res.json({ success: false, message: 'Username already exists' });
+        } else {
+            console.log(`Password: ${password}, Salt Rounds: ${saltRounds}`);
 
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const user = {
-            first_name,
-            last_name,
-            address,
-            login: {
-                username:username,
-                email: email,
-                password: hashedPassword
-            },
-            cards: []
-        };
-        const { resource } = await container.items.create(user);
-        res.json({ success: true, message: 'Account created successfully', id: resource.id });
-    }
-});
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            const user = {
+                first_name,
+                last_name,
+                address,
+                login: {
+                    username: username,
+                    email: email,
+                    password: hashedPassword
+                },
+                cards: []
+            };
+            const { resource } = await container.items.create(user);
+            res.json({ success: true, message: 'Account created successfully', id: resource.id });
+        }
+    });
 
 // Add credit card endpoint
 app.post('/add_credit_card', async (req, res) => {
@@ -109,7 +118,7 @@ app.post('/add_credit_card', async (req, res) => {
     console.log(crypto.randomBytes(16))
     if (!card.number) {
         res.status(400).send({ error: 'Card number is required' });
-      } else {
+    } else {
         const cipher = crypto.createCipheriv(algorithm, key1, iv);
         let encrypted = cipher.update(card.number.toString(), 'utf8', 'hex');
         encrypted += cipher.final('hex');
@@ -121,7 +130,7 @@ app.post('/add_credit_card', async (req, res) => {
     if (card.reward_type) newCard.reward_type = card.reward_type;
     if (card.reward_value) newCard.reward_value = card.reward_value;
     if (card.cardholderName) newCard.cardholderName = card.cardholderName;
-    
+
     resource.cards.push(newCard);
     await container.item(id).replace(resource);
     res.json({ success: true, message: 'Card added successfully' });
@@ -158,9 +167,54 @@ app.post('/logout', (req, res) => {
     });
 });
 
+app.get('/ai_call',async (req, res) => {
+
+    const { questionText, programArray } = req.body;
+
+    const messages = [
+        {
+            role: "system",
+            content: "You are an expert at finding the highest possible credit card rewards given purchases that are made. You talk to people in a confident and knowledgeable tone, like a friend."
+        },
+        {
+            role: "user",
+            content: questionText + ` I have the following credit cards available ${[...programArray]}`
+        },
+    ];
+
+    console.log(`Message: ${messages.map((m) => m.content).join("\n")}`);
+
+    const events = await aiClient.streamChatCompletions(deploymentId, messages, { 
+      maxTokens: 1024,
+      azureChatExtensionOptions: {
+        extensions: [
+          {
+            endpoint: searchEndpoint,
+            key: searchKey,
+            indexName: searchIndex,
+          },
+        ],
+      },
+    });
+    let response = "";
+    for await (const event of events) {
+      for (const choice of event.choices) {
+        const newText = choice.delta?.content;
+        if (!!newText) {
+          response += newText;
+          // To see streaming results as they arrive, uncomment line below
+            //console.log(newText);
+        }
+      }
+    }
+    console.log(response);
+    res.json(response);
+
+});
+
 
 // Start the server
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Server running on port ${port}`));
 
-module.exports =  { app, server };
+module.exports = { app, server };
